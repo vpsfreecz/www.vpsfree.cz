5ed084076670d11a3cbd0720e8a0c71f82b63242
Jakub Skokan <jakub.skokan@havefun.cz>
2014-11-04 14:15:59 +0100

    Prihlaska: remove mail indentation

--- htdocs/prihlaska.php
+++ htdocs/prihlaska.php
@@ -170,21 +170,20 @@ Year of birth: '.$_POST["birth"].'
 
 	// Mail applicant
 	$subject = "[vpsFree.cz] Přihláška přijata";
-	$text = '
-	Ahoj '.$_POST["nick"].',
-	
-	Tvá přihláška byla přijata a bude předložena radě sdružení ke schválení. Do 24 hodin Tě budeme kontaktovat.
-	Pokud by se tak nestalo, obrať se prosím na podpora@vpsfree.cz.
-	
-	Mezitím doporučujeme, aby sis prošel důkladněji náš web na https://www.vpsfree.cz.
-	Další informace, které nezbytně potřebuješ vědět, jsou na https://kb.vpsfree.cz/doku.php?id=sdruzeni:novacci.
-	A konečně, na naší Knowledge Base je kolekce krátkych návodů, které jsou pro vpsFree specifické, je dobré o nich aspoň mít přehled:
-	https://kb.vpsfree.cz
-	
-	Vážíme si Tvého zájmu,
-	
-	vpsFree.cz
-	';
+	$text = 'Ahoj '.$_POST["nick"].',
+
+Tvá přihláška byla přijata a bude předložena radě sdružení ke schválení. Do 24 hodin Tě budeme kontaktovat.
+Pokud by se tak nestalo, obrať se prosím na podpora@vpsfree.cz.
+
+Mezitím doporučujeme, aby sis prošel důkladněji náš web na https://www.vpsfree.cz.
+Další informace, které nezbytně potřebuješ vědět, jsou na https://kb.vpsfree.cz/doku.php?id=sdruzeni:novacci.
+A konečně, na naší Knowledge Base je kolekce krátkych návodů, které jsou pro vpsFree specifické, je dobré o nich aspoň mít přehled:
+https://kb.vpsfree.cz
+
+Vážíme si Tvého zájmu,
+
+vpsFree.cz
+';
 	
 	mail($_POST["email"], $subject, $text, $headers);
 
